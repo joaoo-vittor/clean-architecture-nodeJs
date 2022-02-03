2eae30ca73c4431938a536373b303b36d7384b22
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-02 21:23:14 -0300

    ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -98,7 +98,7 @@ describe('Login Router', () => {
 
   it('Should return 200 when valid credentials are provided', () => {
     // system under test
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
     const httpRequest = {
       body: {
         email: 'valid_email@email.com',
@@ -108,6 +108,7 @@ describe('Login Router', () => {
 
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
   })
 
   it('Should return 500 if no AuthUseCase is provided', () => {
