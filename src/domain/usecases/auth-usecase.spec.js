f35140fdba5e821241188bd55d1574adf7431556
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-03 16:25:14 -0300

    ensure AuthUseCase throws if no dependency is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -84,6 +84,12 @@ describe('Auth UseCase', () => {
     expect(promise).rejects.toThrow()
   })
 
+  it('Should throw if no dependency is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@email.com', 'any_passwod')
+    expect(promise).rejects.toThrow()
+  })
+
   it('Should throw if no LoadUserByEmailRepository has no load method', async () => {
     const sut = new AuthUseCase({ loadUserByEmailRepository: {} })
     const promise = sut.auth('any_email@email.com', 'any_passwod')
