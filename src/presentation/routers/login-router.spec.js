3ba7ca5cffb755c106c675e2cc9644b1fbfd3710
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-03 09:21:43 -0300

    move error to a custom folder

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,8 +1,10 @@
 const LoginRouter = require('./login-router')
-const MissingParamError = require('../helpers/missing-param-error')
-const Unauthorized = require('../helpers/unauthorized-error')
-const InvalidParamError = require('../helpers/invalid-param-error')
-const ServerError = require('../helpers/server-error')
+const {
+  InvalidParamError,
+  MissingParamError,
+  ServerError,
+  UnauthorizedError
+} = require('../errors')
 
 const makeSut = () => {
   const authUseCaseSpy = makeAuthUseCase()
@@ -136,7 +138,7 @@ describe('Login Router', () => {
 
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(401)
-    expect(httpResponse.body).toEqual(new Unauthorized())
+    expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
 
   it('Should return 200 when valid credentials are provided', async () => {
