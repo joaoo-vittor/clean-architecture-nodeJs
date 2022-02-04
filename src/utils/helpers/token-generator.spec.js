d48b0fe6373b8a733aca84d24e334d215de4d053
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-03 23:04:31 -0300

    ensure TokenGenerator returns a token if JWT returns token

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,14 +1,22 @@
+const jwt = require('jsonwebtoken')
 
 class TokenGenerator {
   async generate (id) {
-    return null
+    return jwt.sign(id, 'secret')
   }
 }
 
 describe('Token Generator', () => {
   it('Should return null if JWT retuns null', async () => {
     const sut = new TokenGenerator()
+    jwt.token = null
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
+  })
+
+  it('Should return a token if JWT retuns token', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBe(jwt.token)
   })
 })
