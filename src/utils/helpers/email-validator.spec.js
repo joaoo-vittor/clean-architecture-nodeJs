8fd290ef7c10fdfaac80635dca5a4df047c0978c
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-03 22:37:00 -0300

    ensure EmailValidator throws if no email is provided

--- src/utils/helpers/email-validator.spec.js
+++ src/utils/helpers/email-validator.spec.js
@@ -1,5 +1,6 @@
 const validator = require('validator')
 const EmailValidator = require('./email-validator')
+const { MissingParamError } = require('../errors')
 
 const makeSut = () => {
   return new EmailValidator()
@@ -23,5 +24,10 @@ describe('Email Validator', () => {
     const sut = makeSut()
     sut.isValid('any_email@gmail.com')
     expect(validator.email).toBe('any_email@gmail.com')
+  })
+
+  it('Should throw if no email is provided', async () => {
+    const sut = makeSut()
+    expect(() => { sut.isValid() }).toThrow(new MissingParamError('email'))
   })
 })
