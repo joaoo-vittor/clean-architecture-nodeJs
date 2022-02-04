3a3743b6003d26d8f292b199c910dfd384232ce6
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-03 22:14:22 -0300

    move files to helpers dir

---
+++ src/utils/helpers/email-validator.spec.js
@@ -0,0 +1,27 @@
+const validator = require('validator')
+const EmailValidator = require('./email-validator')
+
+const makeSut = () => {
+  return new EmailValidator()
+}
+
+describe('Email Validator', () => {
+  it('Should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isEmailValid = sut.isValid('valid_email@email.com')
+    expect(isEmailValid).toBe(true)
+  })
+
+  it('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = makeSut()
+    const isEmailValid = sut.isValid('invalid_email@email.com')
+    expect(isEmailValid).toBe(false)
+  })
+
+  it('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@gmail.com')
+    expect(validator.email).toBe('any_email@gmail.com')
+  })
+})
