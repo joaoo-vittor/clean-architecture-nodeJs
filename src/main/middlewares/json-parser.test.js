6b384a05d388349feea477e650383f1f17bbbd66
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-06 15:25:40 -0300

    ensure requests parse body as JSON

---
+++ src/main/middlewares/json-parser.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest')
+const app = require('../config/app')
+
+describe('JSON Parser Middleware', () => {
+  it('should parse body as JSON', async () => {
+    app.post('/test_json_parser', (req, res) => {
+      return res.send(req.body)
+    })
+    await request(app).post('/test_json_parser')
+      .send({ name: 'joão' })
+      .expect({ name: 'joão' })
+  })
+})
