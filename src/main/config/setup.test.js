7bfab73419cdc2727eca9ab2aef9e6889c47d9c3
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-06 15:12:45 -0300

    enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -9,4 +9,14 @@ describe('App Setup', () => {
     const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
+
+  it('should enable CORS', async () => {
+    app.get('/test_x_powered_by', (req, res) => {
+      return res.send('')
+    })
+    const res = await request(app).get('/test_x_powered_by')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
