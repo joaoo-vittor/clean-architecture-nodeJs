b2f0f814a572f76c225642c2c2357bb4d6094caf
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-06 21:33:57 -0300

    move loginRouter composition to its own file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURL: process.env.MONGO_URL || 'mongodb+srv://root:prNl9dfkLXYZcgKt@cluster0.hozmf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  mongoURL: process.env.MONGO_URL || 'mongodb+srv://root:prNl9dfkLXYZcgKt@cluster0.hozmf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
