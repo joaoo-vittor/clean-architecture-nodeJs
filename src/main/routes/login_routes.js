ab3b79eec37926c1fadeef79fa2c28ec17078028
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-06 21:40:31 -0300

    add express router adapter

--- src/main/routes/login_routes.js
+++ src/main/routes/login_routes.js
@@ -1,5 +1,6 @@
 const loginRouter = require('../composers/login-router-composer')
+const expressRouterAdpter = require('../adpters/express-router-adpter')
 
 module.exports = (router) => {
-  router.post('/login', loginRouter)
+  router.post('/login', expressRouterAdpter.adapt(loginRouter))
 }
