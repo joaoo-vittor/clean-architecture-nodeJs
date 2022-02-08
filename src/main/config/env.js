e2021a79e30c695b4680962718f8907820f4dbf9
joaoo-vittor <joaoo.vittor007@gmail.com>
2022-02-08 11:46:51 -0300

    add login route integration test to ensure 200 response when valid credentials are provided

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURL: process.env.MONGO_URL,
+  mongoURL: process.env.MONGO_URL || 'mongodb+srv://root:prNl9dfkLXYZcgKt@cluster0.hozmf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   tokenSecret: process.env.TOKEN_SECRET || 'secret',
   port: process.env.PORT || 5858
 }
---
+++ src/main/routes/login-router.test.js
@@ -0,0 +1,37 @@
+const request = require('supertest')
+const bcrypt = require('bcrypt')
+const MongoHelper = require('../../infra/helpers/mongo-helper')
+const app = require('../config/app')
+let userModel
+
+describe('Login Routes', () => {
+  jest.setTimeout(3000)
+
+  beforeAll(async () => {
+    await MongoHelper.connect()
+    userModel = await MongoHelper.getCollection('users')
+  })
+
+  beforeEach(async () => {
+    await userModel.deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('Should return 200 when valid credentials are provided', async () => {
+    await userModel.insertOne({
+      email: 'valid_email@email.com',
+      password: bcrypt.hashSync('hashed_password', 10)
+    })
+
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'valid_email@email.com',
+        password: 'hashed_password'
+      })
+      .expect(200)
+  })
+})
